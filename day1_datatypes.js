19246b8875aa5939cd5acd154945c2e84898fcb0
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-09-08 00:50:12 +0000

    solved day 1 challenge

---
+++ day1_datatypes.js
@@ -0,0 +1,18 @@
+function dataTypes() {
+    // Declare second integer, double, and String variables.
+    var j = parseInt("12");
+    var e = parseInt("4.0");
+    var t = "is the best place to learn and practice coding!";
+    // Read and save an integer, double, and String to your variables.
+    
+    // Print the sum of both integer variables on a new line.
+    console.log(i + j);
+    // Print the sum of the double variables on a new line.
+    var sumx = d+e;
+    console.log(sumx.toFixed(1));
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    console.log(s + t);
+}    
+
+dataTypes();
